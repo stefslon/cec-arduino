6983fd7630f45f03201d5c93023ddcf729f1e72e
stefslon <stefslon@gmail.com>
2015-04-29 21:18:25 -0400

    Initial commit (mostly as is from Google Code

---
+++ CEC_Arduino.ino
@@ -0,0 +1,78 @@
+#include "CEC_Device.h"
+
+#define IN_LINE 2
+#define OUT_LINE 3
+
+CEC_Device device(0x1000);
+
+bool XX_GetLineState()
+{
+  int state = digitalRead(IN_LINE);
+  return state == LOW;
+}
+
+void XX_SetLineState(CEC_Device* device, bool state)
+{
+  digitalWrite(OUT_LINE, state?LOW:HIGH);
+  // give enough time for the line to settle before sampling
+  // it
+  delayMicroseconds(50);
+  device->_lastLineState2 = XX_GetLineState();
+}
+
+void setup()
+{
+  pinMode(OUT_LINE, OUTPUT);
+  pinMode(IN_LINE, INPUT);
+
+  digitalWrite(OUT_LINE, LOW);
+  delay(200);
+
+  Serial.begin(115200);
+  device.MonitorMode = true;
+  device.Promiscuous = true;
+  device.Initialize(CEC_LogicalDevice::CDT_PLAYBACK_DEVICE);
+}
+
+void loop()
+{
+  if (Serial.available())
+  {
+    unsigned char c = Serial.read();
+    unsigned char buffer[3];
+    
+    Serial.print(" Read: ");
+    Serial.println(char(c));
+    
+    switch (c)
+    {
+      case '1':
+        DbgPrint("<Image View On>\r\n");
+        buffer[0] = 0x04;
+        device.TransmitFrame(0, buffer, 1);
+        
+        DbgPrint("<Set Stream Path >\r\n"); // tv HDMI1, Reciever HDMI4 selected
+        buffer[0] = 0x82; 
+        buffer[1] = 0x14;
+        buffer[2] = 0x00;
+        device.TransmitFrame(15, buffer, 3);
+      
+      case '2':
+        DbgPrint("<Standby>\r\n");
+        buffer[0] = 0x36;   // <Standby>
+        device.TransmitFrame(15, buffer, 1);
+        break;
+        
+      case 'w':
+        DbgPrint("<Send OSD name>\r\n");
+        buffer[0] = 0x47; // <Set OSD Name>
+        memcpy(buffer+1, "Andrew-HTPC", 11);
+        device.TransmitFrame(target, buffer, 12);
+        break; 
+        
+    }
+  }
+  device.Run();
+}
+
+
---
+++ readme.md
@@ -0,0 +1,19 @@
+This code is a compilation of work done by phil123 and AndrewNC
+https://code.google.com/p/cec-arduino/
+Code license: GNU GPL v2
+
+As well as some additional work posted by Biffidus
+over at Arduino forum: http://forum.arduino.cc/index.php?topic=22327.60
+No license posted.
+
+## Overview
+
+This is an Arduino library which implements the HDMI v1.3a CEC wire protocol which allows communication with HDMI CEC capable devices. A typical usage scenario would be a Home Theater PC environment which uses HDMI but does not support CEC. This would allow the HTPC to communicate with other HDMI CEC equipment.
+
+## Circuit
+
+See schematic posted at https://code.google.com/p/cec-arduino/wiki/ElectricalInterface
+
+## Other Resources
+
+For a list of CEC op codes see CEC Table 7 in HDMI CEC 1.3a Spec
